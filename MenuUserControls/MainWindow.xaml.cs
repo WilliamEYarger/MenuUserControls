ab906cfbacce6c77970e9dec141c3dd8e78b5b7c
William Yarger <william.yarger.md@hotmail.com>
2021-03-06 08:36:10 -0500

    Add project files.

---
+++ MenuUserControls/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using MenuUserControls.UserControls;
+
+namespace MenuUserControls
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void home_Click(object sender, RoutedEventArgs e)
+        {
+            SetActiveUserControl(Home);
+        }
+
+        public void SetActiveUserControl(UserControl control)
+        {
+            //Collapse all user controls
+            Home.Visibility = Visibility.Collapsed;
+            News.Visibility = Visibility.Collapsed;
+            Music.Visibility = Visibility.Collapsed;
+            Games.Visibility = Visibility.Collapsed;
+
+            //Make the selected control visible
+            control.Visibility = Visibility.Visible;
+        }
+
+
+        private void menuHome_Click(object sender, RoutedEventArgs e)
+        {
+            SetActiveUserControl(Home);
+        }
+
+        private void menuNnews_Click(object sender, RoutedEventArgs e)
+        {
+            SetActiveUserControl(News);
+        }
+
+        private void menuMusic_Click(object sender, RoutedEventArgs e)
+        {
+            SetActiveUserControl(Music);
+        }
+
+        private void menuGames_Click(object sender, RoutedEventArgs e)
+        {
+            SetActiveUserControl(Games);
+        }
+
+    }
+}
---
+++ MenuUserControls/UserControls/Home.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MenuUserControls.UserControls
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home : UserControl
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFolder_Click(object sender, RoutedEventArgs e)
+        {
+            // Get the Name of the Subject folder
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            string FolderPath = "";
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                FolderPath = dialog.FileName + '\\';
+            }
+            tbkName.Text = FolderPath;
+        }
+    }
+}
